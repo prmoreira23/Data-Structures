8d7ca7d26389f0cf9e70d5b7ff2147a7a1fcd4ce
prmoreira23 <prm.gredes@gmail.com>
2018-09-09 15:10:00 -0400

    Add contains and remove methods to Singly Linked List

--- Singly Linked List/SinglyLinkedList.rb
+++ Singly Linked List/SinglyLinkedList.rb
@@ -16,4 +16,40 @@ class SinglyLinkedList
       @tail = node
     end
   end
+
+  def contains(value)
+    node = @head
+    while node && node.value != value
+      node = node.next
+    end
+    node == nil ? false : true
+  end
+
+  def remove(value)
+    return false unless @head
+    node = @head
+    if node.value == value
+      if @head == @tail
+        @head = nil
+        @tail = nil
+      else
+        @head = @head.next
+      end
+      return true
+    end
+
+    while node.next && node.next.value != value
+      node = node.next
+    end
+
+    if node.next
+      if node.next == @tail
+        @tail = node
+      end
+      node.next = node.next.next
+      return true
+    end
+
+    return false
+  end
 end
