47d63822c30513ddb67ca2eef65ea7e1ebdebd81
prmoreira23 <prm.gredes@gmail.com>
2018-09-09 21:54:53 -0400

    Add find_node to BinarySearchTree

--- Binary Search Tree/BinarySearchTree.rb
+++ Binary Search Tree/BinarySearchTree.rb
@@ -3,6 +3,7 @@ require_relative "Node"
 class BinarySearchTree
   def initialize
     @root = nil
+    @count = 0
   end
 
   def insert(value)
@@ -11,6 +12,7 @@ class BinarySearchTree
     else
       insert_node(@root, value)
     end
+    @count += 1
   end
 
   def contains(value)
@@ -18,10 +20,21 @@ class BinarySearchTree
   end
 
   def remove(value)
-    
+    node_to_remove = find_node(@root, value)
   end
 
   private
+  def find_node(root, value)
+    return nil unless root
+    if root.value == value
+      return root
+    elsif value < root.value
+      return find_node(root.left, value)
+    else
+      return find_node(root.right, value)
+    end
+  end
+
   def contain(root, value)
     return false unless root
     if root.value == value
